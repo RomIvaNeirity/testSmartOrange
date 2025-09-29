b86144b9fbdc5d05796ceeb231f9680b5b638831
Roman <neirity@gmail.com>
2025-09-30 00:24:28 +0300

    section object

--- js/fetchData.js
+++ js/fetchData.js
@@ -3,11 +3,10 @@ import axios from 'https://cdn.jsdelivr.net/npm/axios@1.6.8/+esm';
 export async function getData() {
     try {
         const res = await axios.get('https://test.smarto.agency/smarto_complexes_list.json');
-        console.log(res.data);
-        
         return res.data;
     } catch 
         (error)  { console.log(error) }
-        
+         return [];
         
     }
+
--- js/main.js
+++ js/main.js
@@ -1,14 +1,96 @@
 import { swiper } from './slider.js';
-import axios from 'https://cdn.jsdelivr.net/npm/axios@1.6.8/+esm';
 import { getData } from './fetchData.js';
 
+let startIndex = 0;
+let endIndex = 3;
+let currentType = [];
+let allObject = [];
+let comfortObjects = [];
+let businessObjects = [];
+let premiumObjects = [];
+const container = document.querySelector('.obj-card-container');
+const loadMoreBtn = document.querySelector('.load-more-btn');
+const curentSelection = document.querySelectorAll(".obj-type-item");
 
+async function init() {
+    allObject = await getData();
+    comfortObjects = allObject.filter(obj => obj.type === "Comfort")
+    businessObjects = allObject.filter(obj => obj.type === "Бізнес")
+    premiumObjects = allObject.filter(obj => obj.type === "Premium");
+    return { allObject, comfortObjects, businessObjects, premiumObjects };
+}
 
+init().then(({ allObject, comfortObjects, businessObjects, premiumObjects }) => {
+    
+    curentSelection.forEach(button => {
+        button.addEventListener('click', handleClick);
+    });
+    currentType = allObject
+    const startCards = currentType.slice(startIndex, endIndex);
+    createMarckup(startCards);
+    curentSelection[0].style.textDecoration = 'underline';
+    
+    function handleClick(event) {
+        startIndex = 0;
+        endIndex = 3;
+        const type = event.currentTarget.dataset.type;
 
+        document.querySelectorAll('.obj-type-item')
+        .forEach(el => el.style.textDecoration = 'none');
 
+        event.currentTarget.style.textDecoration = 'underline';
+        
 
-getData()
+        if (type === "all") { currentType = allObject };
+        if (type === "comfort") { currentType = comfortObjects };
+        if (type === "business") { currentType = businessObjects };
+        if (type === "premium") { currentType = premiumObjects };
+       
+        const startCards = currentType.slice(startIndex, endIndex);
+        createMarckup(startCards);
+        loadMoreBtn.style.display = 'block'
+    }
+    
+})
 
+loadMoreBtn.addEventListener("click", event => { 
+       startIndex = startIndex + 3;
+    endIndex = endIndex + 3;
+    if (endIndex >= currentType.length)
+    { loadMoreBtn.style.display = 'none' };
+        let nexstCards = currentType.slice(startIndex, endIndex);
+       
+    createMarckup(nexstCards, 'append');
+    
+    const objCard = document.querySelector(".obj-card-container");
+    const domRect = objCard.getBoundingClientRect();
+    window.scrollBy({ top: (domRect.height), behavior: "smooth" })
+     
+    
+ })
 
+    function createMarckup(arrayObj, method = "inner") {
+        let list = '';
+        const marckUp = arrayObj.map(obj => {
+            list = list + `
+    <div class="card">
+<div class="card-header">
+<p>${obj.year} р.</p>
+<p>${obj.type}</p>
+</div>
+        <img src=${obj.img} width="416" height="234"/>
+        <h3>${obj.name}</h3>
+        <p>${obj.adress}<p>
+    </div>
+    `
+        }
+        ).join("");
 
- 
+        if (method === 'inner') {
+            container.innerHTML = list;
+        } else if (method === 'append') {
+            container.insertAdjacentHTML('beforeend', list);
+        } else {
+            console.error(`Невідомий метод рендеру: ${method}`);
+        }
+    }
